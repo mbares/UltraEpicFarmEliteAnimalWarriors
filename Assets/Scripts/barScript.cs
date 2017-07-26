86e0af288351d78f3c74b86cc6446f5e3c57edfd
Mateo Bares <Mateo Bares>
2017-07-26 21:05:36 +0200

    Imported healthbar prefab made in LootRules game

---
+++ Assets/Scripts/barScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class barScript : MonoBehaviour {
+     
+    private GameObject player;   
+    private float fillAmount;   
+    private Image content;   
+    private Text statusText;
+    private int maxHealth;
+
+    private void Start()
+    {
+        maxHealth = player.GetComponent<CharacterStats>().health;
+    }
+
+    void Update () 
+    {      
+        HealthBar();       
+	}
+
+    void HealthBar()
+    {        
+        statusText.text = player.GetComponent<CharacterStats>().health.ToString();
+        fillAmount = player.GetComponent<CharacterStats>().health / 100;
+        content.fillAmount = fillAmount;
+        if (player.GetComponent<CharacterStats>().health <= 0)
+        {
+            statusText.text = "DEAD";
+        }        
+    }
+   
+ }
